61e1972310501ba585fd4088007f3f9d23a740c5
Daisuke Nishioka <tchon@ucsd.edu>
2021-05-09 15:34:01 -0700

    Added surface-level html and JS obj-definitions
    Co-authored-by: Etienne Robin <Etienne0601@users.noreply.github.com>

---
+++ source/Daisuke-Dev/object list.js
@@ -0,0 +1,35 @@
+var Bullet = {
+  date = someDate,
+  text = privateText,
+  bulletType = 'blackDot',
+  parent = null,
+  tag = [tag1, tag2],
+};
+
+var Note = {
+  date = someDate,
+  text = someText,
+  tag = [tag1, tag2],
+};
+
+var Task = {
+  title = aTitle,
+  date = someDate,
+  desc = someDescription,
+  status = 'completed', //Also includes "pending" and "not started" maybe
+  tag = [tag1, tag2],
+};
+
+var Tracker = {
+  title = aTitle,
+  dateCreated = someDate,
+  nodes: [trackerNode1, trackerNode2],
+  scale = 'month', //Also includes day, week, and year maybe.
+  tag = [tag1, tag2],
+};
+
+var TrackerNode = {
+  x = someX,
+  date = someDate,
+  desc = someDesc,
+};
