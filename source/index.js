51043996303bd7eb48717d55fbdcafda3bfce086
alanlwang1 <alanwangsd@gmail.com>
2021-05-09 20:42:13 -0700

    test commit

---
+++ source/index.js
@@ -0,0 +1,14 @@
+/**
+ * Example function to test CI/CD pipeline
+ * @param {number} a - operand 1 to sum
+ * @param {number} b - operand 2 to sum
+ * @returns {number} Sum of a and b 
+ */
+function sum(a, b) {
+    return a + b;
+}
+
+let a = 1;
+let b = 2;
+let c = sum(a, b); 
+console.log(c);
