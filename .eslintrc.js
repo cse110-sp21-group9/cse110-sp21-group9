b8741a88603e710d560ef3592a713a22352cb8bf
alanlwang1 <alanwangsd@gmail.com>
2021-05-15 11:35:10 -0700

    Add style guide to linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "rules": {
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+    "semi": ["error","always"],
+    "space-before-function-paren": ["error", "never"],
+    "no-var": "error"
+  }
 };
