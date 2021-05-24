77a42f12afb3c5432aba4eb200900c1fe2db8fbe
alanlwang1 <alanwangsd@gmail.com>
2021-05-23 19:59:49 -0700

    update test names

--- test/calendar-month.test.js
+++ test/calendar-month.test.js
@@ -19,48 +19,48 @@ describe('Basic unit tests for monthly calendar ', () => {
 		require('../source/frontend/app/page-calendar/sidebar.js');
 	
   });
-	// DOM Unit Test 1: Check for correct current month/year
-	test("Test1: Calendar has correct current month/year", () => {
+	// DOM Unit Test 1: Page has correct current month/year
+	test("Test1: Page has correct current month/year", () => {
 		const month = document.getElementById('month');
 		const year = document.getElementById('year');
     expect(month.innerHTML).toMatch(monthNames[currMonth]);
 		expect(year.innerHTML).toMatch(currYear.toString()); 
 	});
 
-	// DOM Unit Test 2: Check calendar next month 
-	test("Test2: Check calendar correct next month", () => {
+	// DOM Unit Test 2: Next month button moves calendar to correct month
+	test("Test2: Next month button moves calendar to correct month", () => {
 		const forwardMonth = document.getElementById("forwardmonth");
 		forwardMonth.click(); 
 		const month = document.getElementById('month');
     expect(month.innerHTML).toMatch(monthNames[currMonth + 1]);
 	}); 
 
-	// DOM Unit Test 3: Check calendar previous month
-	test("Test3: Check calendar correct previous month", () => {
+	// DOM Unit Test 3: Previous month button moves calendar to correct month
+	test("Test3: Previous month button moves calendar to correct month", () => {
 		const backMonth = document.getElementById("backmonth");
 		backMonth.click(); 
 		const month = document.getElementById('month');
     expect(month.innerHTML).toMatch(monthNames[currMonth]);
 	}); 
 
-  // DOM Unit Test 4: Check calendar next year
-	test("Test4: Check calendar correct next year", () => {
+  // DOM Unit Test 4: Next year button moves calendar to correct year 
+	test("Test4: Next year button moves calendar to correct year ", () => {
 		const forwardYear = document.getElementById("forwardyear");
 		forwardYear.click(); 
 		const year = document.getElementById('year');
     expect(year.innerHTML).toMatch((currYear + 1).toString());
 	}); 
 
-	// DOM Unit Test 5: Check calendar previous year
-	test("Test5: Check calendar correct next year", () => {
+	// DOM Unit Test 5: Previous year button moves calendar to correct year
+	test("Test5: Previous year button moves calendar to correct year", () => {
 		const backYear = document.getElementById("backyear");
 		backYear.click(); 
 		const year = document.getElementById('year');
     expect(year.innerHTML).toMatch(currYear.toString());
 	}); 
 
-	// DOM Unit Test 6: Check calendar lower month bound (should change year)
-	test("Test6: Check previous month in January (should change to December of last year)", () => {
+	// DOM Unit Test 6: Previous month button in January moves to correct month/year
+	test("Test6:Previous month button in January moves to correct month/year (should change to December of last year)", () => {
 		const backMonth = document.getElementById("backmonth");
 		for(let i = 0; i < currMonth + 1; i++) {
 			backMonth.click(); 
@@ -70,8 +70,8 @@ describe('Basic unit tests for monthly calendar ', () => {
     expect(month.innerHTML).toMatch("December");
 		expect(year.innerHTML).toMatch((currYear - 1).toString());
 	}); 
-	// DOM Unit Test 7: Check calendar upper month bound (should change year) 
-	test("Test7: Check next month in December (should change to January of next year)", () => {
+	// DOM Unit Test 7: Next month button in December moves to correct month/year 
+	test("Test7: Next month button in December moves to correct month/year (should change to January of next year)", () => {
 		const forwardMonth = document.getElementById("forwardmonth");
 		for(let i = 0; i < 13; i++) {
 			forwardMonth.click(); 
