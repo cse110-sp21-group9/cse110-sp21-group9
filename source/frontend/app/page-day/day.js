4d08a5489e474f8aa9b4f82ce8c276e8a2db77f3
Daisuke Nishioka <tchon@ucsd.edu>
2021-05-26 05:49:55 -0700

    Added url hash and dynamic date loading

--- source/frontend/app/page-day/day.js
+++ source/frontend/app/page-day/day.js
@@ -7,6 +7,9 @@
  *  TODO: Add note bullet functionality
  *  TODO: Integrate with firebase backend
  *  TODO: Update outdated crudFunctions calls
+ *  TODO: Account for updated date format
+ *  TODO: Plan to read url hash: If the hash is empty, just bring us to the current day
+ *        Otherwise, load bullets from the date in the urlhash
  */
 
 /* get elements from html page */
@@ -48,8 +51,43 @@ const editTags = document.getElementById('edittag');
 
 const timeSegments = document.getElementById('time_list');
 
+const monthNames = {
+  1: 'January',
+  2: 'Feburuary',
+  3: 'March',
+  4: 'April',
+  5: 'May',
+  6: 'June',
+  7: 'July',
+  8: 'August',
+  9: 'September',
+  10: 'October',
+  11: 'November',
+  12: 'December'
+};
+
+const week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
 // make sure this changes depending on the day you pick
-const curDay = '2020-04-01';
+let defMonth = '04';
+let defYear = '2021';
+let defDay = '01';
+let timeStart = 'T00:00';
+let timeEnd = 'T23:59';
+// generate hash of the day we're in if we don't have one already
+const hashed = generateHash();
+// update the date variables we have accordingly and the url
+[defMonth, defYear, defDay] = getCurrentDay(hashed);
+updateURL(hashed);
+// set the UI to display the current day
+setDay([defMonth, defYear, defDay]);
+let curDay = defYear + '-' + defMonth + '-' + defDay;
+console.log(curDay);
+// generate date+time strings to load bullets from
+let loadStart = curDay + timeStart;
+let loadEnd = curDay + timeEnd;
+console.log(loadStart);
+console.log(loadEnd);
 // POTENTIALLY OLD
 crud.initCrudRuntime();
 
@@ -85,7 +123,7 @@ function makeTimeSlotComponent(intTime) {
 // TODO: Write a date getter function to pass into here
 // TODO: Make sure the date here is all the bullets of the current day we're in
 // POTENTIALLY OLD
-const bulletsToLoad = crud.getBulletsByDateRange('2020-04-01T00:00', '2020-04-01T23:59');
+const bulletsToLoad = crud.getBulletsByDateRange(loadStart, loadEnd);
 for (const bullet of bulletsToLoad) {
   if (bullet.type === 'note') {
     // paste bullet on notespace
@@ -93,16 +131,21 @@ for (const bullet of bulletsToLoad) {
   } else {
     for (let hourIt = 0; hourIt < 24; hourIt++) {
       const hours = document.querySelectorAll('[class = "time_slot"]');
-      const curHour = hours[hourIt];
+      let curHour = hours[hourIt];
+      console.log(curHour);
       let bulHour = bullet.date.toString();
-      bulHour = bulHour.substring(11, 13);
+      bulHour = bulHour.substring(10, 12);
       console.log("Bullet's Hour: " + bulHour);
-      if (bulHour === hourIt) {
+      let bulHourNum = parseInt(bulHour);
+      if (bulHourNum === hourIt) {
+        console.log('pasting bullet from storage');
         const list = curHour.querySelector('ul');
         if (bullet.type === 'event') {
           list.append(createBulletEntryElem(bullet.ID));
+          break;
         } else if (bullet.type === 'task') {
           list.append(createBulletEntryElem(bullet.ID));
+          break;
         }
       }
     }
@@ -318,4 +361,109 @@ function createBulletEntryElem(intBulletID) {
   });
 
   return newEntry;
+}
+
+/** Generates a month, year, and day given a url hash
+ *  @param {string} hash the url hash
+ *  @return the month, year, and day that we're in
+ */
+function getCurrentDay(hash) {
+  let curMonth;
+  let curYear;
+  let curDay;
+
+  //parse the hash
+  const curr = hash.split('?');
+  curMonth = parseInt(curr[0].split('=')[1]);
+  console.log(curMonth);
+  curYear = parseInt(curr[1].split('=')[1]);
+  curDay = parseInt(curr[2].split('=')[1]);
+
+  //return the date
+  console.log('Month: ' + curMonth + ' ' + 'Year: ' + curYear + ' ' + 'Day: ' + curDay);
+  return [curMonth, curYear, curDay];
+}
+
+/** TODO: Make this function work with new hash system
+ *  Generates a hash based on either: 
+ *  1. The URL we inherited from
+ *  2. Today's date
+ *  3. A default date
+ *  @param {boolean} onload tells us whether we are inheriting a URL or not
+ *  @return a hash that tells us which month, year, and day we're in
+ */
+function generateHash(onload = true) {
+  let curr = document.URL;
+  if (onload) {
+    let month;
+    let year;
+    let day;
+    if (curr.includes('#')) {
+      curr = document.URL.split('#')[1].split('?');
+      month = parseInt(curr[0].split('=')[1]);
+      year = parseInt(curr[1].split('=')[1]);
+      day = parseInt(curr[2].split('=')[1]);
+      return '#month=' + month + '?year=' + year + '?day=' + day;
+    } else {
+      let date = new Date();
+      date = date.toISOString().split('T')[0].split('-');
+      month = parseInt(date[1]);
+      year = parseInt(date[0]);
+      day = parseInt(date[2]);
+      return '#month=' + month + '?year=' + year + '?day=' + day;
+    }
+  } else {
+    return '#month=' + defMonth + '?year=' + defYear + '?day=' + defDay;
+  }
+}
+
+/** Updates the url of the page we are in based on the given hash
+ *  @param {string} hash the hash we want to update our url with
+ *  @return null
+ */
+function updateURL(hash) {
+  const url = new URL(document.URL);
+  url.hash = hash;
+  document.location.href = url.href;
+}
+
+/** Sets the day view's date-related variables
+ *  NOTE: params may actually be in string format rather than int
+ *  @param {Number} month the month of the current day
+ *  @param {Number} year the year of the current day
+ *  @param {Number} day the day of the current day
+ *  @param {Number} week the weekday of the current day
+ *  @return null
+ */
+function setDay([month, year, day]) {
+  let curMonth = month;
+  let curYear = year;
+  let curDay = day;
+  let monthDisplay = document.getElementById('curMonth');
+  monthDisplay.innerHTML = monthNames[curMonth];
+  let dateDisplay = document.getElementById('date')
+  console.log(dateDisplay);
+  dateDisplay.innerHTML = curMonth + '/' + curDay + '/' + curYear;
+  let weekDisplay = document.getElementById('week_day');
+  let weekDay = getWeekday([curMonth, curYear, curDay]);
+  console.log('weekday: ' + weekDay);
+  weekDay = Math.round(weekDay) % 7;
+  console.log(weekDay);
+  weekDisplay.innerHTML = week[weekDay];
+}
+
+/** Gets the current day of the week based on the day's month, 
+ *  day and year.
+ *  Credit for week calculation formula: 
+ *  https://www.geeksforgeeks.org/find-day-of-the-week-for-a-given-date/ 
+ *  @param {String} month the month of hte current day;
+ *  @param {String} year the year of the current day
+ *  @param {String} day the day of the current year
+ *  @return the current weekday in number form
+ */
+function getWeekday([month, year, day]) {
+  let t = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4];
+  let y = year;
+  y -= (month < 3) ? 1 : 0;
+  return (y + y/4 - y/100 + y/400 + t[month-1] + day) % 7;
 }
