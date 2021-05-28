9b5541ce01c30e49bd1ef8ed49f6e09c58a4c76a
Edmund Leibert III <edmund.leibert@gmail.com>
2021-05-27 18:22:52 -0700

    dev: added foundation files for backend

---
+++ source/backend/bullet.js
@@ -0,0 +1,44 @@
+export class Bullet {
+  constructor(objBullet) {
+    this.ID = objBullet.ID; // automatic generation up to nearest second, hidden from user
+    this.title = objBullet.title; // user selection
+    this.type = objBullet.type; // user selection, this is the specific type of bullet
+    this.date = objBullet.date; // user selected date, the one actually displayed to the user
+    this.tags = objBullet.tags; // user selection
+    this.content = objBullet.content; // user input
+    this.dueDate = objBullet.dueDate;
+    this.status = objBullet.status;
+  }
+
+  get ID() {
+    return this.ID;
+  }
+
+  get title() {
+    return this.title;
+  }
+
+  get type() {
+    return this.type;
+  }
+
+  get date() {
+    return this.date;
+  }
+
+  get tags() {
+    return this.tags;
+  }
+
+  get content() {
+    return this.content;
+  }
+
+  get dueDate() {
+    return this.dueDate;
+  }
+
+  get status() {
+    return this.status;
+  }
+}
