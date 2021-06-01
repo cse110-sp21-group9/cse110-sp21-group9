64898e20e685b6d9ede5be8e4740842707de7f17
edwardyang12 <edwardyang016@gmail.com>
2021-06-01 02:46:02 -0700

    brrrrrrrrrrr

--- source/frontend/app/page-settings/script.js
+++ source/frontend/app/page-settings/script.js
@@ -1,82 +1,20 @@
-const testData = {
-  id_1: {
-    type: 'task', // task, event, note
-    title: 'Walk Dog',
-    date: '2021-05-07T20:00'
-  },
-  id_2: {
-    type: 'task',
-    title: 'Feed Dog',
-    date: '2021-05-08T20:00'
-  },
-  id_3: {
-    type: 'event',
-    title: 'Test',
-    date: '2021-05-10T20:00'
-  },
-  id_4: {
-    type: 'event',
-    title: 'Eat',
-    date: '2021-05-07T20:00'
-  },
-  id_5: {
-    type: 'note',
-    title: 'Hi Edmund',
-    date: '2021-05-10T20:00'
-  },
-  id_6: {
-    type: 'note',
-    title: 'heuhuehue',
-    date: '2021-04-28T20:00'
-  },
-  id_7: {
-    type: 'note',
-    title: 'heuhuehue2',
-    date: '2021-05-01T20:00'
-  },
-  id_8: {
-    type: 'note',
-    title: 'mwahahaaha',
-    date: '2021-05-30T20:00'
-  },
-  id_9: {
-    type: 'note',
-    title: 'mwahahaaha2',
-    date: '2021-06-04T20:00'
-  },
-  id_10: {
-    type: 'event',
-    title: 'Walk',
-    date: '2021-05-07T20:00'
-  },
-  id_11: {
-    type: 'event',
-    title: 'Walk2',
-    date: '2021-05-07T20:00'
-  },
-  id_12: {
-    type: 'event',
-    title: 'Walk3',
-    date: '2021-05-07T20:00'
-  },
-  id_13: {
-    type: 'event',
-    title: 'Walk4',
-    date: '2021-05-07T20:00'
-  }
-
-};
-
 const exportData = document.getElementById('exportData');
 const getFiles = document.getElementById('input');
 const data = document.getElementById('data');
 const modeChange = document.getElementById('modeChange');
 
-data.innerHTML = JSON.stringify(testData);
 
 exportData.addEventListener('click', function() {
   const a = document.createElement('a');
-  a.href = URL.createObjectURL(new Blob([JSON.stringify(testData, null, 2)], {
+  var temp = {}; 
+  if (localStorage.getItem('bulletIDs') != null) {
+    var list = JSON.parse(localStorage.getItem('bulletIDs'))["array"];
+    for (let i = 0; i < list.length; i++) {
+      temp[list[i]] = JSON.parse(localStorage.getItem(list[i]));
+    }
+  }
+ 
+  a.href = URL.createObjectURL(new Blob([JSON.stringify(temp, null, 2)], {
     type: 'text/plain'
   }));
   a.setAttribute('download', 'json.txt');
@@ -98,6 +36,19 @@ getFiles.addEventListener('change', function() {
 
   function callBackTest(input) {
     data.innerHTML = input;
+    var large=0;
+    var array = []
+    
+    for (const [key, value] of Object.entries(JSON.parse(input))) {
+      localStorage.setItem(key,JSON.stringify(value));
+
+      if(parseInt(key)>large){
+        large = parseInt(key);
+      }
+      array.push(parseInt(key));
+    }
+    localStorage.setItem('bulletIDs', JSON.stringify({"array": array}));
+    localStorage.setItem('lastID',large);
   }
 
   read(callBackTest, this.files[0]);
